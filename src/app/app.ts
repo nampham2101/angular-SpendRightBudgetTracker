cb34f7e37fe8b604bd99ae881d7bee78a9c2328f
Nam Pham <nam.pham@adnovum.vn>
2026-05-11 10:45:44 +0700

    Add header and content block

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,5 +7,5 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('SpendRightBudgetTracker');
+  protected readonly title = signal('Spend Right - Budget Tracker');
 }
